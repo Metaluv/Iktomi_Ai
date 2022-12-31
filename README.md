9928d6713438b354aa59e2ce9154f1ea05881890
Jason Cardinal <93737375+Metaluv@users.noreply.github.com>
2022-12-31 00:15:11 -0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Iktomi The Storyteller
- A replica of ChatGPT made  with the youtube guide from JavaScript Mastery
+# Build and Deploy Your Own ChatGPT AI Application That Will Help You Code
+![Open AI CodeGPT](https://i.ibb.co/LS4DRhb/image-257.png)
+
+### Launch your development career with project-based coaching - https://www.jsmastery.pro
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config()
-console.log(process.env.OPENAI_API_KEY)
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -17,7 +17,7 @@ app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Tansi from Iktomi!'
+    message: 'Hello from CodeX!'
   })
 })
 
